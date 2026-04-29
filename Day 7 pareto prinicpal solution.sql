ff0458195c82d52e6bab1779956c726dfd579252
Sanjana Jadhav <141809386+SanjanaJadhav2802@users.noreply.github.com>
2026-04-29 11:46:14 +0400

    29 april 2026

---
+++ Day 7 pareto prinicpal solution.sql
@@ -0,0 +1,36 @@
+﻿/*The Pareto Principle (80/20 rule) states that 80% of outcomes often come from 20% of causes.
+
+In retail analytics, this commonly translates to:
+80% of total sales come from 20% of the products
+You are given an orders table containing sales transactions for multiple products across several years.
+
+🎯 Objective
+Write a SQL query to identify the top 20% of products that cumulatively contribute to at least 80% of total sales.
+
+📋 Table Structure
+
+CREATE TABLE orders (
+    order_id   VARCHAR(14),
+    order_date DATE,
+    product_id VARCHAR(15),
+    sales      NUMERIC(9,4)
+);
+*/
+/*
+--Find out 80% of Total Sale
+select sum(sales) * 0.8 from orders
+Answer: 1837760.77711996
+*/
+
+--Find out highest saling products and keep it in desc order and Create CTE
+WITH product_wise_sales as(
+select product_id,sum(sales) as product_sales
+from orders 
+group by product_id
+)
+select product_id,product_sales
+,sum(product_sales) over (order by product_sales desc rows between unbounded preceding and 0 preceding) as running_sales
+,0.8*sum(product_sales) over () as total_sales
+from product_wise_sales
+
+
---
+++ Day 8 Friends score.sql
@@ -0,0 +1,34 @@
+--Friends score
+--Total friends score should be greater than 100
+
+Create table friend (pid int, fid int);
+insert into friend (pid , fid ) values ('1','2');
+insert into friend (pid , fid ) values ('1','3');
+insert into friend (pid , fid ) values ('2','1');
+insert into friend (pid , fid ) values ('2','3');
+insert into friend (pid , fid ) values ('3','5');
+insert into friend (pid , fid ) values ('4','2');
+insert into friend (pid , fid ) values ('4','3');
+insert into friend (pid , fid ) values ('4','5');
+
+create table person (PersonID int,	Name varchar(50),	Score int);
+insert into person(PersonID,Name ,Score) values('1','Alice','88');
+insert into person(PersonID,Name ,Score) values('2','Bob','11');
+insert into person(PersonID,Name ,Score) values('3','Devis','27');
+insert into person(PersonID,Name ,Score) values('4','Tara','45');
+insert into person(PersonID,Name ,Score) values('5','John','63');
+
+select * from person;
+select * from friend;
+
+with score_details as (
+select f.pid,sum(p.Score) as friends_score,count(*) as total_friends
+from friend f inner join
+person p on p.PersonID = f.fid
+group by f.pid
+having sum(p.Score)>100
+)
+select s.*,p.Name
+from score_details s inner join 
+person p on s.pid =p.PersonID
+
